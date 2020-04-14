04f75151966bc40160da5d2789d1a9dd2bd226f7
Lorna Lynch <lornalynchit@yahoo.com>
2020-04-14 19:49:39 -0400

    updated files

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,6 @@ router.post("/api/transaction/bulk", ({body}, res) => {
 });
 
 router.get("/api/transaction", (req, res) => {
-
   Transaction.find({}).sort({date: -1})
     .then(dbTransaction => {
       res.json(dbTransaction);
--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-
 const PORT = 3000;
 
 const app = express();
-
 
 app.use(logger("dev"));
 
@@ -16,17 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-let connectionURI;
-if (process.env.MONGODB_URI) {
-  console.log(process.env);
-  console.log(process.env.MONGODB_URI);
-  connectionURI=process.env.MONGODB_URI;
-}
-  else {
-    connectionURI="mongodb://localhost/budget";
-  }
-console.log(connectionURI)
-mongoose.connect(connectionURI, {
+
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
